1d5459b43bc24eb451a6198626865611056047ec
Alejandro Reyes <arcaela.reyes@gmail.com>
2023-04-24 06:03:09 -0400

    - - -

--- src/scripts/use-template/index.ts
+++ src/scripts/use-template/index.ts
@@ -51,21 +51,35 @@ export default new Command({
         },
     },
     async action(options) {
-        options.type ||= await prompt("What do you want? \"project\" or \"module\"") as "module" | "project"
-        options.name ||= await prompt("Where you want to create?") as string
-        if (!options.name) throw new Error("Name could not be empty")
+        options.type ||= await prompt("What do you want create a \"project\" or \"module\"?") as "module" | "project"
         if (options.type !== 'project' && options.type !== 'module')
             throw new Error(`Type must be \"module\" or \"project\", received: ${options.type}`)
+        options.name ||= await prompt(`What do you want to name your ${options.type}?`) as string
+        if (!options.name)
+            throw new Error("Name could not be empty")
 
         const target = path.join(process.cwd(), options.name)
         const source = path.join(__dirname, 'templates', options.type)
-        console.log('source: %s target: %', source, target)
-        // if (fs.existsSync(target))
-        //     fs.rmSync(target, { recursive: true })
-        // copy(source, target)
+        if (fs.existsSync(target))
+            fs.rmSync(target, { recursive: true })
+        copy(source, target)
 
-        // shell.cd(target)
-        // shell.exec('npx yarn --force')
-        // process.exit()
+        shell.cd(target)
+        const install = await prompt("Do you want to install the dependencies now? [N]")
+        if (install.match(/^y.*/i)) {
+            const providers = {
+                "npm": "npm ci",
+                "yarn": "yarn install --frozen-lockfile",
+                "pnpm": "pnpm ci",
+            }
+            const provider = await prompt("What package manager do you want to use? [" + Object.keys(providers).join("] [") + "]")
+            if (provider in providers)
+                await shell.exec(providers[provider])
+            else
+                console.log("You selected a provider that is not supported, but you can still continue the installation manually.")
+            process.exit(0)
+        }
+        shell.exec('npx yarn --force')
+        process.exit()
     },
 })
