0e4ae662755717a1a3d67598cbc38fb6c85eb901
Alejandro Reyes <arcaela.reyes@gmail.com>
2023-04-24 05:11:16 -0400

    - - -

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,37 @@
 import "colors"
 import fs from "node:fs"
 import path from "node:path"
+import Command from "@arcaelas/command"
 
 const scripts = fs.readdirSync(
     path.join(__dirname, 'scripts')
 )
-const [command, ...args] = process.argv.slice(
+const [script, ...args] = process.argv.slice(
     process.argv.indexOf(__filename) + 1
 )
 
-if (!scripts.includes(command)) {
-    console.log("Arcaelas CLI".underline.green)
-    console.log("   arcaela [command] [arguments?] --help?")
-    console.log("%s is not a valid command", command.red.bold)
-    console.log("commands:")
-    console.log("        %s", scripts.join(" "))
-    process.exit()
-}
-
-import(
-    path.resolve(__dirname, 'scripts', command)
-).then(({ default: script }) => script.exec(args))
-
+new Command({
+    arguments: {
+        name(token: string) {
+            if (!scripts.includes(token)) {
+                console.log("Arcaelas CLI".underline.green)
+                console.log("   arcaela [command] [arguments?] --help?")
+                console.log("%s is not a valid command", script.red.bold)
+                console.log("commands:")
+                console.log("        %s", scripts.join(" "))
+                process.exit()
+            }
+            return token
+        },
+        arguments: Array,
+    },
+    async action(options, argv) {
+        const module = await import(
+            path.resolve(__dirname, 'scripts', options.name)
+        )
+        return module.default(options.arguments, argv)
+    },
+}).exec({
+    name: script,
+    arguments: args,
+})
